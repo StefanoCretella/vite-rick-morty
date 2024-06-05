d5258c77559efd52dbba2033846c9011f0634302
StefanoCretella <cretellastefano213@gmail.com>
2024-06-05 16:36:51 +0200

    [UPD] (esercizio martedi 4 giugno) search function added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
     <Header />
+    <div class="search-container">
+      <input v-model="searchName" placeholder="Search by name" />
+      <select v-model="searchStatus">
+        <option value="">Select Status</option>
+        <option value="alive">Alive</option>
+        <option value="dead">Dead</option>
+        <option value="unknown">Unknown</option>
+      </select>
+      <button @click="searchCharacters">Search</button>
+      <button @click="resetSearch">Reset</button>
+    </div>
     <CharacterList :characters="characters" />
   </div>
 </template>
@@ -13,13 +24,32 @@ export default {
   components: { Header, CharacterList },
   data() {
     return {
-      characters: []
+      characters: [],
+      searchName: '',
+      searchStatus: ''
     };
   },
   async created() {
-    const response = await fetch('https://rickandmortyapi.com/api/character');
-    const data = await response.json();
-    this.characters = data.results;
+    this.fetchCharacters();
+  },
+  methods: {
+    async fetchCharacters() {
+      const response = await fetch('https://rickandmortyapi.com/api/character');
+      const data = await response.json();
+      this.characters = data.results;
+    },
+    async searchCharacters() {
+      const nameQuery = this.searchName ? `&name=${this.searchName}` : '';
+      const statusQuery = this.searchStatus ? `&status=${this.searchStatus}` : '';
+      const response = await fetch(`https://rickandmortyapi.com/api/character?${nameQuery}${statusQuery}`);
+      const data = await response.json();
+      this.characters = data.results;
+    },
+    resetSearch() {
+      this.searchName = '';
+      this.searchStatus = '';
+      this.fetchCharacters();
+    }
   }
 }
 </script>
@@ -38,5 +68,11 @@ body {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.search-container {
+  display: flex;
+  gap: 1rem;
+  margin: 1rem 0;
 }
 </style>
